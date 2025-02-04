7f4533b5d36314e7e40f5d3f915883fc5fdbd9f5
alain.clinquart <alain.clinquart@economie.fgob.be>
2025-02-04 15:50:24 +0100

    ajout exemple mapping data typescript on html

--- src/app/face-snap/face-snap.component.ts
+++ src/app/face-snap/face-snap.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-face-snap',
@@ -6,6 +6,20 @@ import { Component } from '@angular/core';
   templateUrl: './face-snap.component.html',
   styleUrl: './face-snap.component.scss'
 })
-export class FaceSnapComponent {
+export class FaceSnapComponent implements OnInit {
 
+  title: string="";
+  description: string="";
+  createdAt: Date= new Date();
+  snaps: number=1;
+  imageUrl: string="";
+
+  ngOnInit() {
+    this.title = 'Archibald';
+    this.description = 'Mon meilleur ami depuis toujours !';
+    this.createdAt = new Date();
+    this.snaps = 5;
+    
+    this.imageUrl = 'angular.png';
+  }
 }
